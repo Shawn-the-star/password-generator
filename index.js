11a2768de591f7c253f53d006f5fa79c1eaf5fa8
Shawn <81163565+Shawn-the-star@users.noreply.github.com>
2021-12-18 11:55:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+class Password {
+	constructor(digits = 24) {
+		this.digits = digits
+	}
+
+	generate() {
+		var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		var lowercase = "abcdefghijklmnopqrstuvwxyz";
+		var num = "0123456789";
+		var symbols = "`~!@#$%^&*()-_=+[{]};:\'\",<.>/?\\";
+		var comb = [
+			uppercase,
+			lowercase,
+			num, 
+			symbols
+		]
+
+		var id = "";
+
+		for (let x = 0; x < this.digits; x++) {
+			var randCom = Math.floor(Math.random() * comb.length)
+			var randPlace = Math.floor(Math.random() * comb[randCom].length)
+			id += comb[randCom][randPlace]
+		}
+		return id;
+	}
+}
+
+const pass = new Password(24).generate()
+console.log(pass)
